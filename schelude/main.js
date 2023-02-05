ab9df630cf5834e909ebec77a9a25f440033f874
JetGreen <grishadzyin@gmail.com>
2023-02-05 09:15:07 +0400

    fix ch

--- schelude/main.js
+++ schelude/main.js
@@ -1,8 +1,22 @@
 const schedule = require('node-schedule');
 
+const { ProblemModel } = require('../models/problem-model.js')
+const CompanyModel = require('../models/company-model.js')
+
 module.exports = function setupScheludes() {
     // ADD CHECKING PROBLEMS
-    const job = schedule.scheduleJob('11 * * * *', () => { // run every hour at minute 1
-        console.log('The answer to life, the universe, and everything!');
+    const checkProblemsJob = schedule.scheduleJob('1 * * * *', async () => { // run every hour at minute 1
+        let allProblems = await ProblemModel.find({})
+        let COMPANY = await CompanyModel.findOne({ identifier: '0' })
+
+        for (let PROBLEM of allProblems) {
+            let lastAction = PROBLEM.actions[PROBLEM.actions.length - 1]
+            switch (lastAction.status) {
+                case 'created':
+                    if ((Date.now() - (lastAction.date / 1000 / 60 / 60).toFixed(0)) >= 24) {
+                        // найти ответсвенного в иерархии компании и присвоить его id в respEmpl
+                    }
+            }
+        }
     });
 }
