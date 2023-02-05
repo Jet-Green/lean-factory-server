1516998bb9b6766880e8e0eace09d2cee243e077
Jet-Green <grishadzyin@gmail.com>
2023-02-05 12:28:35 +0400

    add problem propagation

--- schelude/main.js
+++ schelude/main.js
@@ -2,20 +2,59 @@ const schedule = require('node-schedule');
 
 const { ProblemModel } = require('../models/problem-model.js')
 const CompanyModel = require('../models/company-model.js')
+const { EmplModel } = require('../models/empl-model.js')
 
 module.exports = function setupScheludes() {
     // ADD CHECKING PROBLEMS
-    const checkProblemsJob = schedule.scheduleJob('1 * * * *', async () => { // run every hour at minute 1
+    const checkProblemsJob = schedule.scheduleJob('1 * * * * *', async () => { // run every hour at minute 1
         let allProblems = await ProblemModel.find({})
-        let COMPANY = await CompanyModel.findOne({ identifier: '0' })
 
         for (let PROBLEM of allProblems) {
             let lastAction = PROBLEM.actions[PROBLEM.actions.length - 1]
+            let emplWithTrueHierarchy;
+            // -1 потому что там уже есть один нужный нам статус
+            let depth = -1
+
             switch (lastAction.status) {
                 case 'created':
-                    if ((Date.now() - (lastAction.date / 1000 / 60 / 60).toFixed(0)) >= 24) {
-                        // найти ответсвенного в иерархии компании и присвоить его id в respEmpl
+                    for (let a of PROBLEM.actions) {
+                        if (a.status == 'created') {
+                            if (!emplWithTrueHierarchy) emplWithTrueHierarchy = a.respEmpl
+                            depth++;
+                        }
                     }
+
+                    if ((Date.now() - (lastAction.date / 1000 / 60 / 60).toFixed(0)) >= 0) {
+                        let firstRespEmpl = await EmplModel.findById(emplWithTrueHierarchy)
+                        let nextRespEmpl = firstRespEmpl != null ? firstRespEmpl.hierarchy?.up[depth]?._id : null
+
+                        // найти ответсвенного в иерархии компании и присвоить его id в respEmpl
+                        if (nextRespEmpl) {
+                            PROBLEM.actions.push({ status: 'created', respEmpl: nextRespEmpl, date: Date.now() })
+                            await EmplModel.findOneAndUpdate({ _id: nextRespEmpl }, { $push: { reportsToFix: PROBLEM._id } })
+                            await PROBLEM.save()
+                        }
+                    }
+                    break;
+                case 'sent_to_fix':
+                    for (let a of PROBLEM.actions) {
+                        if (a.status == 'sent_to_fix') {
+                            if (!emplWithTrueHierarchy) emplWithTrueHierarchy = a.respEmpl
+                            depth++;
+                        }
+                    }
+
+                    if ((Date.now() - (lastAction.date / 1000 / 60 / 60).toFixed(0)) >= 0) {
+                        let firstRespEmpl = await EmplModel.findById(emplWithTrueHierarchy)
+                        let nextRespEmpl = firstRespEmpl != null ? firstRespEmpl.hierarchy?.up[depth]?._id : null
+
+                        if (nextRespEmpl) {
+                            PROBLEM.actions.push({ status: 'sent_to_fix', respEmpl: nextRespEmpl, date: Date.now() })
+                            await EmplModel.findOneAndUpdate({ _id: nextRespEmpl }, { $push: { reportsToFix: PROBLEM._id } })
+                            await PROBLEM.save()
+                        }
+                    }
+                    break;
             }
         }
     });
